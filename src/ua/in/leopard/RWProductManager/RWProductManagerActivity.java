57971d36b820d7276f4b9c779f218a1995d82218
Alexey <leopard.not.a@gmail.com>
2011-11-04 01:21:48 +0200

    add libs

--- src/ua/in/leopard/RWProductManager/RWProductManagerActivity.java
+++ src/ua/in/leopard/RWProductManager/RWProductManagerActivity.java
@@ -1,13 +1,13 @@
 package ua.in.leopard.RWProductManager;
 
-import android.app.Activity;
+import com.phonegap.*;
 import android.os.Bundle;
 
-public class RWProductManagerActivity extends Activity {
+public class RWProductManagerActivity extends DroidGap {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        super.loadUrl("file:///android_asset/www/android/index.html");
     }
 }
