58c7e0aef2b6cab837c17231c35270a7e69591b0
Alexey <leopard.not.a@gmail.com>
2011-11-04 00:32:05 +0200

    first commit

---
+++ src/ua/in/leopard/RWProductManager/RWProductManagerActivity.java
@@ -0,0 +1,13 @@
+package ua.in.leopard.RWProductManager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RWProductManagerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
